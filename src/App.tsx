5499e0d8d3ce316db8fa32aa4913f548c686dc14
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-27 10:33:11 +0200

    product card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import Card from "./components/Card/Card";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="main-container">
       <Header title="xxx" btn1="xxxx" btn2="xxxx" />
+      <Card SKU="xxxx" Name="Product" Price={10} Props="Weight: 10" />
       <Footer />
     </div>
   );
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import "./Card.scss";
+
+interface Props {
+  SKU: string;
+  Name: string;
+  Price: number;
+  Props: string;
+}
+
+const Card: FC<Props> = ({ SKU, Name, Price, Props }) => {
+  return (
+    <div className=" ProductCard ">
+      <div className="card-content d-flex flex-column justify-content-center align-items-center">
+        <label htmlFor={SKU}>{SKU}</label>
+        <h4>{Name}</h4>
+        <span>{Price} $</span>
+        <span>{Props}</span>
+      </div>
+      <div className="card-check">
+        <input type="checkbox" id={SKU} />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
