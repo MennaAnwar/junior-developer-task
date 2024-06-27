b00217dfc8d1c51acc69fd6aa4f1918f87802019
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-06-27 12:05:16 +0200

    2 decimal place adjusted for price

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -14,7 +14,7 @@ const Card: FC<Props> = ({ SKU, Name, Price, Props }) => {
       <div className="card-content d-flex flex-column justify-content-center align-items-center">
         <label htmlFor={SKU}>{SKU}</label>
         <h4>{Name}</h4>
-        <span>{Price} $</span>
+        <span>{Price.toFixed(2)} $</span>
         <span>{Props}</span>
       </div>
       <div className="card-check">
